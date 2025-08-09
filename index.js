e678600364bd995ee70af88bff524221665d981b
Mujtaba <mujtabaabid06@gmail.com>
2025-08-09 21:41:30 +0500

    ok love

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+require("dotenv").config()
 
-app.get("/", (req, res) => {
-    res.send("this is me mujtaba abid");
-});
+const app = express()
+const port = 3000
 
-app.get("/facebook", (req, res) => {
-    res.send("this is Facebook path");
-});
 
-app.get("/login", (req, res) => {
-    res.send("<h1>this is login page</h1>");
-});
+app.get("/" ,(req,res)=>{
+    res.send("   this is me mujtaba baid ")
+})
 
-app.get("/youtube", (req, res) => {
-    res.send("<h1>this is YouTube</h1>");
-});
+app.get("/facebook" ,(Req,res)=>{
+    res.send("this is faceBook path ")
+})
 
-module.exports = app;
+app.get("/login" ,(req,res)=>{
+    res.send("<h1>this is login page</h1> ")
+})
+
+app.get("/youtube", (req,res)=>{
+    res.send("<h1> this is you tube </h1>")
+})
+app.listen(process.env.port, ()=>{
+    console.log("ye mne bbnaya he listen")
+})
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+

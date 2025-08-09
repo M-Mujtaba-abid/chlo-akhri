be955f9f2e6e034be5d73bf9b06e713b46239bad
Mujtaba <mujtabaabid06@gmail.com>
2025-08-09 21:37:32 +0500

    vercel cahnges kiye hein file bnaye he

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
-const express = require('express')
-require("dotenv").config()
+const express = require('express');
+const app = express();
 
-const app = express()
-const port = 3000
+app.get("/", (req, res) => {
+    res.send("this is me mujtaba abid");
+});
 
+app.get("/facebook", (req, res) => {
+    res.send("this is Facebook path");
+});
 
-app.get("/" ,(req,res)=>{
-    res.send("   this is me mujtaba baid ")
-})
+app.get("/login", (req, res) => {
+    res.send("<h1>this is login page</h1>");
+});
 
-app.get("/facebook" ,(Req,res)=>{
-    res.send("this is faceBook path ")
-})
+app.get("/youtube", (req, res) => {
+    res.send("<h1>this is YouTube</h1>");
+});
 
-app.get("/login" ,(req,res)=>{
-    res.send("<h1>this is login page</h1> ")
-})
-
-app.get("/youtube", (req,res)=>{
-    res.send("<h1> this is you tube </h1>")
-})
-app.listen(process.env.port, ()=>{
-    console.log("ye mne bbnaya he listen")
-})
-app.listen(process.env.port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
+module.exports = app;

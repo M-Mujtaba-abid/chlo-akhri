1c62a50ecc87639d7f0f3853cf5766896fdd4765
Mujtaba <mujtabaabid06@gmail.com>
2025-08-09 21:19:07 +0500

    first commit

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-// const express = require('express')
-// require("dotenv").config()
+const express = require('express')
+require("dotenv").config()
 
-// const app = express()
-// const port = 3000
-
-
-// app.get("/" ,(req,res)=>{
-//     res.send("   this is me mujtaba baid ")
-// })
-
-// app.get("/facebook" ,(Req,res)=>{
-//     res.send("this is faceBook path ")
-// })
-
-// app.get("/login" ,(req,res)=>{
-//     res.send("<h1>this is login page</h1> ")
-// })
-
-// app.get("/youtube", (req,res)=>{
-//     res.send("<h1> this is you tube </h1>")
-// })
-// app.listen(process.env.port, ()=>{
-//     console.log("ye mne bbnaya he listen")
-// })
-// app.listen(process.env.port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+const app = express()
+const port = 3000
 
 
+app.get("/" ,(req,res)=>{
+    res.send("   this is me mujtaba baid ")
+})
 
-const express = require('express');
-require("dotenv").config();
+app.get("/facebook" ,(Req,res)=>{
+    res.send("this is faceBook path ")
+})
 
-const app = express();
+app.get("/login" ,(req,res)=>{
+    res.send("<h1>this is login page</h1> ")
+})
 
-app.get("/", (req, res) => {
-    res.send("this is me mujtaba abid");
-});
+app.get("/youtube", (req,res)=>{
+    res.send("<h1> this is you tube </h1>")
+})
+app.listen(process.env.port, ()=>{
+    console.log("ye mne bbnaya he listen")
+})
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
-app.get("/facebook", (req, res) => {
-    res.send("this is Facebook path");
-});
 
-app.get("/login", (req, res) => {
-    res.send("<h1>this is login page</h1>");
-});
 
-app.get("/youtube", (req, res) => {
-    res.send("<h1>this is YouTube</h1>");
-});
+// const express = require('express');
+// require("dotenv").config();
 
-// Vercel will handle listening
-module.exports = app;
+// const app = express();
+
+// app.get("/", (req, res) => {
+//     res.send("this is me mujtaba abid");
+// });
+
+// app.get("/facebook", (req, res) => {
+//     res.send("this is Facebook path");
+// });
+
+// app.get("/login", (req, res) => {
+//     res.send("<h1>this is login page</h1>");
+// });
+
+// app.get("/youtube", (req, res) => {
+//     res.send("<h1>this is YouTube</h1>");
+// });
+
+// // Vercel will handle listening
+// module.exports = app;

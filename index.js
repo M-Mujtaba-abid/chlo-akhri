b6836f9989f1130d75420dd2e7adae46f8cb5ebb
Mujtaba <mujtabaabid06@gmail.com>
2025-08-09 20:07:45 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+require("dotenv").config()
+
+const app = express()
+const port = 3000
+
+
+app.get("/" ,(req,res)=>{
+    res.send("   this is me mujtaba baid ")
+})
+
+app.get("/facebook" ,(Req,res)=>{
+    res.send("this is faceBook path ")
+})
+
+app.get("/login" ,(req,res)=>{
+    res.send("<h1>this is login page</h1> ")
+})
+
+app.get("/youtube", (req,res)=>{
+    res.send("<h1> this is you tube </h1>")
+})
+app.listen(process.env.port, ()=>{
+    console.log("ye mne bbnaya he listen")
+})
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
